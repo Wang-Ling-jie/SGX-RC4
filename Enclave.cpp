22c07f9f5d7850a28e8f34e27233ba6684578f8a
wanglingjie <3095332731@qq.com>
2023-06-05 19:40:57 +0800

    Add files

---
+++ Enclave.cpp
@@ -0,0 +1,102 @@
+/*
+ * Copyright (C) 2011-2021 Intel Corporation. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *   * Neither the name of Intel Corporation nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+#include "Enclave.h"
+#include "Enclave_t.h" /* print_string */
+#include <stdarg.h>
+#include <stdio.h> /* vsnprintf */
+#include <string.h>
+
+/* 
+ * printf: 
+ *   Invokes OCALL to display the enclave buffer to the terminal.
+ */
+int printf(const unsigned char* fmt, ...)
+{
+    unsigned char buf[BUFSIZ] = { '\0' };
+    va_list ap;
+    va_start(ap, fmt);
+    vsnprintf(buf, BUFSIZ, fmt, ap);
+    va_end(ap);
+    ocall_print_string(buf);
+    return (int)strnlen(buf, BUFSIZ - 1) + 1;
+}
+
+
+// RC4 decryption implementation in Intel SGX
+
+unsigned char* key = reinterpret_cast<const unsigned char*>("gosecgosec");
+unsigned char K[256];
+unsigned char S[256];
+unsigned char keystream[256];
+
+void ecall_sbox_generation()
+{
+    for (int i = 0; i < 255; ++i)
+    {
+        S[i] = i;
+        K[i] = key[i % 10];
+    }
+    int j = 0;
+    for (int i = 0; i < 255; ++i)
+    {
+        j = (j + S[i] + K[i]) % 256;
+        unsigned char temp = S[i];
+        S[i] = S[j];
+        S[j] = temp;
+    }
+}
+
+void ecall_keystream_generation()
+{
+    int i = 0;
+    int j = 0;
+    for (int k = 0; k < 256; ++k)
+    {
+        i = (i + 1) % 256;
+        j = (j + S[i]) % 256;
+        unsigned char temp = S[i];
+        S[i] = S[j];
+        S[j] = temp;
+        int t = (S[i] + S[j]) % 256;
+        keystream[k] = S[t];
+    }
+}
+
+void ecall_decryption(unsigned char* ciphertext, unsigned char* plaintext, size_t len)
+{
+    const char* temp = reinterpret_cast<const char*>(ciphertext);
+    for (int i = 0; i < strlen(temp); ++i)
+    {
+        plaintext[i] = ciphertext[i] ^ keystream[i];
+    }
+    printf("plaintext: %s\n", plaintext);
+}
